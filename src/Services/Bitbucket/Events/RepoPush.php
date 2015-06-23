1c03780e911a7fe0a6409e8298489c9159d8c972
Mikron <mikronia@gmail.com>
2015-06-23 16:54:16 +0200

    BitBucket service rewritten to new structure, issue management added

--- src/Services/Bitbucket/Events/RepoPush.php
+++ src/Services/Bitbucket/Events/RepoPush.php
@@ -9,38 +9,26 @@ use FP\Larmo\Agents\WebHookAgent\Services\Bitbucket\EventInterface;
  * @package FP\Larmo\Agents\WebHookAgent\Services\Bitbucket\Events
  *
  * Handles pushed commits
- * Caution: BitBucket is not providing entirety of data in push report, only the last commit and information regarding previous HEAD
- * Therefore, this event will not report all commits in a push, just the most recent one; this appears to be intentional behaviour of BitBucket API
+ * Caution: BitBucket is not providing entirety of data in push report, only the last commit and information regarding
+ * previous HEAD. Therefore, this event will not report all commits in a push, just the most recent one; this appears
+ * to be intentional behaviour of BitBucket API
+ *
  */
-class Push implements EventInterface
+class RepoPush extends EventAbstract
 {
-    private $messages;
-
-    public function __construct($data)
-    {
-        $this->messages = $this->prepareMessages($data);
-    }
-
-    public function getMessages()
-    {
-        return $this->messages;
-    }
-
-    private function prepareMessages($data)
+    protected function prepareMessages($data)
     {
         $messages = array();
 
         foreach ($data->push->changes as $change) {
-            array_push($messages, $this->getArrayFromCommit($change));
+            array_push($messages, $this->getArrayFromCommit($change->new->target));
         }
 
         return $messages;
     }
 
-    private function getArrayFromCommit($change)
+    private function getArrayFromCommit($commit)
     {
-        $commit = $change->new->target;
-
         return array(
             'type' => 'bitbucket.commit',
             'timestamp' => strtotime($commit->date),
