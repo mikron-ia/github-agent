9b84dd2eeac860f10419fae3ff6b1c88929fce94
Mikron <mikronia@gmail.com>
2015-08-12 10:46:04 +0200

    Event base classes rework; phase 2.4. - method split applied to trello and scrutinizer

--- src/Services/Scrutinizer/ScrutinizerData.php
+++ src/Services/Scrutinizer/ScrutinizerData.php
@@ -13,37 +13,10 @@ class ScrutinizerData extends ServiceAbstract
     protected function prepareData($data)
     {
         if ($data->state === 'completed') {
-            $diff = $data->_embedded->index_diff;
-
-            $message = array(
-                'type' => 'scrutinizer.' . $data->state,
-                'timestamp' => $data->finished_at,
-                'author' => array(),
-                'body' => 'Scrutinizer CI inspection',
-                'extras' => array(
-                    'id' => $data->uuid,
-                    'repository' => array(
-                        'user' => isset($data->metadata->source) ? $data->metadata->source->login : $data->_embedded->repository->login,
-                        'branch' => isset($data->metadata->source) ? $data->metadata->source->branch : $data->metadata->branch,
-                        'name' => isset($data->metadata->source) ? $data->metadata->source->repository : $data->_embedded->repository->name
-                    ),
-                    'pull_request_number' => isset($data->pull_request_number) ? $data->pull_request_number : '',
-                    'status' => $data->build->status,
-                    'results' => array(
-                        'new_issues' => $diff->nb_new_issues,
-                        'common_issues' => $diff->nb_common_issues,
-                        'fixed_issues' => $diff->nb_new_issues,
-                        'added_code_elements' => $diff->nb_added_code_elements,
-                        'common_code_elements' => $diff->nb_common_code_elements,
-                        'changed_code_elements' => $diff->nb_changed_code_elements,
-                        'removed_code_elements' => $diff->nb_removed_code_elements
-                    )
-                )
-            );
+            $event = new ScrutinizerEvent($data);
+            return $event->getMessages();
         } else {
             throw new EventTypeNotFoundException;
         }
-
-        return array($message);
     }
 }
---
+++ src/Services/Travis/TravisEvent.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace FP\Larmo\Agents\WebHookAgent\Services\Travis;
+
+
+class TravisEvent extends EventAbstract
+{
+    protected function prepareRepositoryData($repository)
+    {
+        return null;
+    }
+
+    protected function prepareType($data)
+    {
+        return 'travis';
+    }
+
+    protected function prepareBody($data)
+    {
+        return  'The Travis CI build';
+    }
+
+    protected function prepareTimeStamp($data)
+    {
+        return $data->finished_at;
+    }
+
+    protected function prepareAuthor($data)
+    {
+        return [
+            'name' => $data->committer_name,
+            'email' => $data->committer_email
+        ];
+    }
+
+    protected function prepareExtras($data)
+    {
+        $attributes = $data->object_attributes;
+        return [
+            'build_url' => $data->build_url,
+            'number_build' => $data->number,
+            'type' => $data->type,
+            'state' => $data->state,
+            'git_number' => $data->type == "push" ? $data->commit : $data->pull_request_number,
+            'git_url' => $data->compare_url,
+            'repository' => array(
+                'name' => $data->repository->name,
+                'owner' => $data->repository->owner_name,
+                'branch' => $data->branch
+            )
+        ];
+    }
+}
